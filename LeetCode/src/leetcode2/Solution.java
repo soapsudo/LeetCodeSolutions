2b8340a581307c717e893d8d489f8b5eddc147b7
BlazOstojic <blaz.ostoji22@gmail.com>
2025-02-08 14:16:14 +0100

    [Solutions] Solved problems 1, 2 and 3.

---
+++ LeetCode/src/leetcode2/Solution.java
@@ -0,0 +1,40 @@
+package leetcode2;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+
+        ListNode result = addTwoNumbers(l1, l2);
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+
+        return dummyHead.next;
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Leet Code solutions
+- A repository to store all of my (hopefully) final Leet Code solutions in.
